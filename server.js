8083221df3e0e3b1d05f21c0f9ac1a5bd330a3e1
Timothy Allgood <timothyallgood@gmail.com>
2020-07-22 13:46:29 -0500

    added database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+require('dotenv').config();
 
 //____________Controllers_________________//
 const userCtrl = require('./controllers/userController');
